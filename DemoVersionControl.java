daa12dd64e3f11148444508900723d96c8277fa4
Ajith Anand Sadanam <ajith.anand@lxisoft.com>
2017-06-27 17:29:46 +0530

    https://github.com/ajithprasadanand/INCUBATOR/pull/1

---
+++ DemoVersionControl.java
@@ -0,0 +1,28 @@
+/**
+*
+*
+*/
+
+public class DemoVersionControl{
+
+private long versionID ;
+
+
+
+public void setVersionID(long versionID)
+{
+	this.versionID = versionID ;
+}
+
+
+public long getVersionID()
+{
+	 return versionID ;
+}
+
+
+
+
+
+
+}
